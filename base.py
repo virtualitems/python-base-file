cbaab782371cd4b6118d8d8908c7653b117a595b
Virtual Items <virtualitemsuniverse@gmail.com>
2023-08-24 09:27:37 -0500

    created base.py

---
+++ base.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+docstring
+"""
+
+# standard
+from typing import TYPE_CHECKING
+from logging import getLogger
+
+# framework
+
+# third-party
+
+# first-party
+
+# typing
+if TYPE_CHECKING:  # pragma: no cover
+    pass
+
+# logging
+logger = getLogger(__name__)
